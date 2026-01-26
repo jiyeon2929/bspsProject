5c6e298436e0400a9e0572907643641922826354
User <User@HYW>
2026-01-26 14:53:38 +0900

    수정

--- src/com/bsps/main/controller/Main.java
+++ src/com/bsps/main/controller/Main.java
@@ -1,10 +1,12 @@
 package com.bsps.main.controller;
 
+
+
+// 클래스에는 접근제한자는 public 과 default를 사용한다. public 클래스는 파일명과 같아야만 한다.
+// 팀 프로젝트 시 PL(프로젝트 리더) 개발
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
+
