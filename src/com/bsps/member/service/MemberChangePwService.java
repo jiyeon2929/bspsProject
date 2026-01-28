e481b017b5747d8267d24a993190f558a83aad1f
User <User@HYW>
2026-01-28 14:23:42 +0900

    회원관리 서비스 수정

--- src/com/bsps/member/service/MemberChangePwService.java
+++ src/com/bsps/member/service/MemberChangePwService.java
@@ -1,13 +1,15 @@
 package com.bsps.member.service;
 
 import com.bsps.main.service.Service;
+import com.bsps.member.dao.MemberDAO;
 
 public class MemberChangePwService implements Service {
 
 	@Override
 	public Object service(Object obj) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		MemberDAO dao = new MemberDAO();
+		return dao;
 	}
 
 }
--- src/com/bsps/member/service/MemberViewService.java
+++ src/com/bsps/member/service/MemberViewService.java
@@ -1,13 +1,18 @@
 package com.bsps.member.service;
 
 import com.bsps.main.service.Service;
+import com.bsps.member.dao.MemberDAO;
+import com.bsps.member.vo.MemberVO;
 
 public class MemberViewService implements Service {
 
 	@Override
 	public Object service(Object obj) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		MemberVO param = (MemberVO) obj;
+		MemberDAO dao = new MemberDAO();
+		
+		return dao.view(param);
 	}
 
 }
